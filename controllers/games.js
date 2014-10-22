7d3939ca102a66a5b42dcb04f470b6f569a6ea5c
Ferran Basora <fcsonline@gmail.com>
2014-10-22 09:25:00 +0200

    Dump sides for queue and history lists

--- controllers/games.js
+++ controllers/games.js
@@ -1,27 +1,42 @@
 module.exports = function (app) {
 
   var Promise = require('bluebird')
+    , _ = require('underscore')
     , bookshelf = app.get('bookshelf')
     , Game = require('../models/game')(bookshelf);
 
-  app.get('/api/games/queue', function (req, res) {
+  function searchGames(res, filter) {
     Game
-    .where('status', 'scheduled')
+    .query(filter)
     .fetchAll()
-    .then(function (models) {
-      res.json(models);
+    .then(function (collection) {
+      return collection.mapThen(function (game) {
+        return Promise.all([game.left().fetchOne(), game.right().fetchOne()]).then(function (results) {
+          game.left = results[0];
+          game.right = results[1];
+          return game;
+        });
+      }).then(function (results) {
+        var output = results.map(function (game) {
+          return (_.extend({}, game.attributes, {
+            left: game.left.id
+          , right: game.right.id
+          }));
+        });
+        res.json(output);
+      });
+    });
+  }
+
+  app.get('/api/games/queue', function (req, res) {
+    searchGames(res, function (qb) {
+      qb.where('status', 'scheduled');
     });
   });
 
   app.get('/api/games/history', function (req, res) {
-    Game
-    .query(function (qb) {
+    searchGames(res, function (qb) {
       qb.whereNotIn('status', ['scheduled', 'playing']);
-    })
-    .fetchAll()
-    .then(function (models) {
-      res.json(models);
     });
   });
-
 };
