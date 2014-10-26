562f56b30b5cbe7e8a55aa546f8365c8b4d7d687
Jorge Morante <jorge@morante.eu>
2014-10-26 18:56:54 +0100

    added react-router

---
+++ webclient/javascripts/views/games.js
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var Games = React.createClass({
+  render: function () {
+    return (
+      <h1>Games</h1>
+    )
+  }
+});
+
+module.exports = Games;
---
+++ webclient/javascripts/views/stats.js
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var Stats = React.createClass({
+  render: function () {
+    return (
+      <h1>Stats</h1>
+    )
+  }
+});
+
+module.exports = Stats;
