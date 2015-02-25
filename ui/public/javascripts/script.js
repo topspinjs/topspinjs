950fd29148d50a8067d5de34d70d44f31b68b7d4
Ferran Basora <fcsonline@gmail.com>
2015-02-25 18:39:58 +0100

    Change the behaviour when the current is not found

--- ui/public/javascripts/script.js
+++ ui/public/javascripts/script.js
@@ -1,9 +1,13 @@
 var socket = io.connect();
 
 function update() {
-  $.getJSON( "/api/games/current", function (data) {
+  $.getJSON( "/api/games/current")
+  .done(function (data) {
     $('.left').html(data.score_left);
     $('.right').html(data.score_right);
+  })
+  .fail(function (data) {
+    $('.left, .right').html('?');
   });
 }
 
