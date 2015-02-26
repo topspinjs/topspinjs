6b484d72948072929694d464e634ac067cc90faa
Ferran Basora <fcsonline@gmail.com>
2015-02-26 18:58:55 +0100

    Remove the ruby dep

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
