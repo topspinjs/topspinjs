bf4c55866992802b4a54ee77f70b8394ac020265
Ferran Basora <fcsonline@gmail.com>
2015-08-20 23:55:33 +0200

    Emit new connection event

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,12 +1,25 @@
 module.exports = function (app, io) {
 
-  var events = app.get('events')
+  var connections = []
+    , events = app.get('events')
+    ,  _ = require('underscore')
+    , uaparse = require('user-agent-parser')
     , chalk = require('chalk');
+
 
   io.on('connection', function (socket) {
     console.log(chalk.green('New connection'));
 
+    events.emit('connections.new', {
+      ip: socket.handshake.address
+    , agent: uaparse(socket.handshake.headers['user-agent'])
+    });
+
+    connections.push(socket);
+
     socket.on('disconnect', function () {
+      connections = _.without(connections, socket);
+
       io.sockets.emit('user disconnected');
     });
   });
@@ -24,6 +37,11 @@ module.exports = function (app, io) {
   events.on('games.new', function (game) {
     console.log(chalk.green('Emit new game'));
     io.emit('games.new', game.toJSON());
+  });
+
+  events.on('connections.new', function (connection) {
+    console.log(chalk.green('Emit new connection'));
+    io.emit('connections.new', connection);
   });
 
 };
